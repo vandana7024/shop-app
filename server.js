5cacb3ad7a0d48005a3cbae503d61c87f01a371d
vandana7024 <vandanadewangan1999@gmail.com>
2022-04-28 18:13:08 +0530

    update user

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // <== Will be created later
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router("./db.json"); // <== Will be created later
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
 const port = process.env.PORT || 5000; // <== You can change the port
 
 server.use(middlewares);
-server.use(router);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
 
-server.listen(port);
+server.use(router);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import { API } from "./constant";
 
 function App() {
   const [users, setUsers] = React.useState([]);
+
   React.useEffect(() => {
-    axios.get("/users").then((res) => {
+    axios.get(`${API}/data`).then((res) => {
       setUsers(res.data);
     });
   }, []);
 
   return (
     <div className="App">
+      ALL USERS
       <div>
         {users?.map((user) => {
           return (
-            <div>
+            <div key={user.id}>
               <div>
                 {user.id}:{user.name}
               </div>
-              <div>{user.email}</div>
             </div>
           );
         })}
